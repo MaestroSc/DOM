1cf427240449d10cd1111d34658ee4a6d1ec17bd
Vitaliy Cherkov <MaestroCrimea@yandex.ua>
2014-01-31 02:21:07 +0200

    Убран id 'popup', оптимизация

--- homework.js
+++ homework.js
@@ -4,22 +4,22 @@
  * @private
  */
 
-document.body.onclick = function(event){
-	if (event.target.className == 'popup-link') _onMouseClick(event);
-}
+document.body.addEventListener('click', function(event) {
+    if (event.target.className == 'popup-link') _onMouseClick(event);
+}, true)
 
-// Переменная e - содержит Event событие клика по ссылке с классом 'popup-link'. 
-// 'e' задана в данном шаблоне функции _onMouseClick, и не изменена :)
 function _onMouseClick(e) { 
         if (e.preventDefault) e.preventDefault(); // Если метод существует, то отменяем его действие
         return openPopupFromLink(e.target);
 }
 
 function setLayer (name, text){ // Блок задания параметров popup вынесен в функцию
-	return '<div class="box-title">' + name + '</div> \
-			<div class="box-message">'+ text + '</div> \
-			<div id="butt"><button>Да</button> \
-			<button>Нет</button></div>';
+	return '<div id="boxM"> \
+				<div class="box-title">' + name + '</div> \
+				<div class="box-message">'+ text + '</div> \
+				<div id="butt"><button>Да</button> \
+				<button>Нет</button></div> \
+			</div>';
 }
 
 /**
@@ -30,29 +30,22 @@ function setLayer (name, text){ // Блок задания параметров 
 
 function openPopupFromLink(link) {
 	var dataTitle = link.dataset.title,
-		dataMessage = link.dataset.message;
+		dataMessage = link.dataset.message.replace(/'%s'/g, link),
+		popupBox = document.body.lastChild; 
 
-	dataMessage = dataMessage.replace(/'%s'/g, link); //Проверяем на спец символ, и заменяем на ссылку
-
-	if (document.getElementById('popup')){
-		document.getElementById('popup').style.display = 'block';
-		document.getElementById('popup').innerHTML = setLayer(dataTitle, dataMessage);
+	if (document.getElementById('boxM')){
+		popupBox.style.display = 'block';
+		popupBox.innerHTML = setLayer(dataTitle, dataMessage);
 	} else{
 		createPopup(dataTitle, dataMessage);
 	}
 
-	/*var b_yes = document.getElementById('butt').firstChild, Первый вариант - обратиться к кнопке черз
-		b_no = document.getElementById('butt').lastChild;     родителя кнопок, div*/
-
-	var b_yes = document.getElementsByTagName('button')[0], //Второй вариант - обращение к кнопкам 
-		b_no = document.getElementsByTagName('button')[1];	//напрямую, через тег.
-
-	b_yes.addEventListener('click',function(){
+	document.getElementsByTagName('button')[0].addEventListener('click',function(){
 		return location.assign(link);
 	}, false);
 
-	b_no.addEventListener('click', function(){ 
-		return document.getElementById('popup').style.display = 'none'
+	document.getElementsByTagName('button')[1].addEventListener('click', function(){ 
+		return document.body.lastChild.style.display = 'none'
 	}, false);
 
 };
@@ -69,6 +62,5 @@ function createPopup(title, message) {
 	var boxMessage = document.createElement('div');
 
 	boxMessage.innerHTML = setLayer(title, message);
-	boxMessage.id = 'popup';
 	return document.body.appendChild(boxMessage);
 };
